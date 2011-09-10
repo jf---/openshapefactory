cf927824f50099b3d30fb084a3501fe4eed83aa6
alex pena <alex pena@localhost>
2011-09-11 03:23:16 +1000

    added minmap documentation

---
+++ doc/openshapefactorymindmap/map.mm
@@ -0,0 +1,36 @@
+<map version="0.9.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node COLOR="#000000" CREATED="1315671790671" ID="ID_92545901" MODIFIED="1315672055444" TEXT="OpenShapeFactory">
+<font NAME="SansSerif" SIZE="20"/>
+<hook NAME="accessories/plugins/AutomaticLayout.properties"/>
+<node COLOR="#0033ff" CREATED="1315671957531" ID="ID_387961071" LINK="#ID_1995655849" MODIFIED="1315672118911" POSITION="left" TEXT="OpenCascade">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1315671843225" ID="ID_1244137904" MODIFIED="1315672055426" TEXT="HybridShapeFactory">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1315672018193" ID="ID_563863075" MODIFIED="1315672055428" POSITION="left" TEXT="QT">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1315671908589" ID="ID_1361654861" MODIFIED="1315672055430" TEXT="2d Viewport">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1315671919589" ID="ID_809908536" MODIFIED="1315672055431" TEXT="GraphicView">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1315671933039" ID="ID_1743584948" MODIFIED="1315672055437" TEXT="QScintilla Editor">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1315671897259" ID="ID_488621916" MODIFIED="1315672055444" TEXT="3d Viewport">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1315672081622" ID="ID_1995655849" MODIFIED="1315672092581" TEXT="QToccViewport">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+</node>
+</node>
+</map>
